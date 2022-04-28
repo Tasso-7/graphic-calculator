7a35d97b662cc8abe628b66c0ea3f93a643abc9a
Tasso-7 <giova.tasselli@gmail.com>
2022-04-28 17:50:28 +0000

    version 0.12

--- project/display.py
+++ project/display.py
@@ -13,9 +13,16 @@ def cartesian_generation(pos_X,pos_Y,sizeX,sizeY):
     goto(pos_X+sizeX,pos_Y)
     goto(pos_X,pos_Y)
 
+def display_function():
+    pass
+
 def showfunction(pos_X,pos_Y,sizeX,sizeY,pixel_value):
     values = {}
+    data = {}
     lastevalue = pixel_value * (0-sizeX)
     for i in range(pos_X-sizeX,pos_X+sizeX+1):
         values[i] = lastevalue
         lastevalue += pixel_value
+    for j,i in values.items():
+        data[j] = elab.elab(i)
+    display_function(data)
