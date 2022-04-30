4530fb14e6139f817a4d76d6a1ea6b6d1e939959
Tasso-7 <giova.tasselli@gmail.com>
2022-04-30 10:31:15 +0000

    version 0.14.2

--- project/elab.py
+++ project/elab.py
@@ -1,31 +1,39 @@
 def elab(operation,num):
     return eval(operation.replace("x",str(num)))
 
+def clamp(num,min,max):
+    if num <= min:
+        return min
+    elif num >= max:
+        return max
+    else:
+        return num
+
 def fixed_operation(operation):
     operations = ["+","-","*","/","%"]
     x = False
     op = False
+    precedent_op = False
     newoperation = ""
     for i in operation:
+        precedent_op = op
         op = False
-        if i == x:
+        for j in operations:
+            if j == i:
+                op = True
+        if i == "x":
             x = True
-            if not op:
+            if not (op or precedent_op):
                 newoperation += "*x"
+            else:
+                newoperation += "x"
         else:
             if not x:
                 newoperation += i
             else:
-                now_op = False
-                for j in operations:
-                    if j == i:
-                        now_op = True
-                if now_op:
+                if op or precedent_op:
                     newoperation += i
                 else:
                     newoperation += "*" + i
-                
-        for j in operations:
-            if j == i:
-                op = True
+                x = False
     return newoperation
